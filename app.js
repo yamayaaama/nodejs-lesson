6e65ebd5c16b67812f54a35e0b34615695acdf41
yamayaaama <yama.yaaama512@gmail.com>
2024-01-25 10:44:20 +0900

    modify PORT

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/api/v1/tasks", taskRoute);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(PORT, console.log("sever started!!"));
+    app.listen(process.env.PORT || PORT, console.log("sever started!!"));
   } catch(err) {
     console.log(err);
   }
